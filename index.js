c88f3f8b0b51de216deb49aaec5c9ea4796511e7
Mine <mine.kandil@gmail.com>
2020-02-12 10:22:19 +0300

    create listener array and subscribe methods

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 function createStore(){
+/*  - the state tree 
+    - a way to get the state tree
+    - a way to listen and respond to the state changing
+    - a way to update the state */
     let state
+    let listeners =[]
 
     const getState = () => state
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
+
+const store = createStore()
+store.subscribe(() =>{
+    console.log('The new state is: ',store.getState())
+})
+store.subscribe(() =>{
+    console.log('The store changed.')
+})
