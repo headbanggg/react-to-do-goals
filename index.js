24feb650fb12a7c9db9e7abb90c1405bf291ebaa
Mine <mine.kandil@gmail.com>
2020-02-12 10:40:54 +0300

    add listeners to array and return listeners

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ function createStore(){
 
     const getState = () => state
 
+    const subscribe = (listener) =>{
+        listeners.push(listener)
+        return () =>{
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
     return {
         getState,
         subscribe
     }
 }
-
-const store = createStore()
-store.subscribe(() =>{
-    console.log('The new state is: ',store.getState())
-})
-store.subscribe(() =>{
-    console.log('The store changed.')
-})
